1f3acc4abe837ec59fd30da06f92ddc91d83b1b3
Zaid Loni <96578621+zaidloni@users.noreply.github.com>
2023-02-16 14:11:16 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -6,6 +6,8 @@ const person = {
     };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
-
+const team = players;
+const team1 = [...players];
+const cap1 = Object.assign({}, person);
 
 export {players,person,team,team1,cap1}
